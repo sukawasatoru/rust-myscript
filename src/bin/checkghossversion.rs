e25ffa1c60cb3eac15a04690c5f9ef5fdbcc6f4d
Satoru Sukawa <sukawasatoru.github@outlook.jp>
2018-11-24 12:30:46 +0900

    Reduce definitions

--- src/bin/checkghossversion.rs
+++ src/bin/checkghossversion.rs
@@ -13,6 +13,7 @@ extern crate toml;
 use std::fs::File;
 use std::io::Read;
 use std::path::PathBuf;
+use serde_json::Value;
 
 include!(concat!(env!("OUT_DIR"), "/checkghossversion_token.rs"));
 
@@ -37,39 +38,6 @@ struct GithubOssConfig {
 #[derive(Debug, Deserialize)]
 struct ResultViewer {
     login: String,
-}
-
-#[derive(Debug, Deserialize)]
-#[serde(rename_all = "camelCase")]
-struct ResultRateLimit {
-    cost: i32,
-    limit: i32,
-    node_count: i32,
-    remaining: i32,
-    reset_at: String,
-}
-
-#[derive(Debug, Deserialize)]
-struct ResultBody {
-    data: ResultData,
-}
-
-#[derive(Debug, Deserialize)]
-#[serde(rename_all = "camelCase")]
-struct ResultData {
-    repository: ResultRepository,
-    viewer: ResultViewer,
-    rate_limit: ResultRateLimit,
-}
-
-#[derive(Debug, Deserialize)]
-struct ResultRepository {
-    releases: ResultReleasesNodes,
-}
-
-#[derive(Debug, Deserialize)]
-struct ResultReleasesNodes {
-    nodes: Vec<ResultRelease>,
 }
 
 #[derive(Debug, Deserialize)]
@@ -112,12 +80,15 @@ fn main() {
 
     for oss in &oss_list.oss {
         let result = retrieve_releases(&oss_list.github.host, &ghtoken, &oss.owner, &oss.name);
-        debug!("result={:?}", result);
-        let stable_list = result.data.repository.releases.nodes.iter()
+        debug!("result={}", result);
+        let mut result_list = serde_json::from_str::<Value>(&result).unwrap();
+        let result_list = result_list["data"]["repository"]["releases"]["nodes"].take();
+        let result_list = serde_json::from_value::<Vec<ResultRelease>>(result_list).unwrap();
+        let stable_list = result_list.iter()
             .filter(|entry| !entry.is_draft && !entry.is_prerelease)
             .take(1)
-            .collect::<Vec<&ResultRelease>>();
-        match stable_list.get(0) {
+            .collect::<Vec<_>>();
+        match stable_list.first() {
             Some(release) => {
                 match oss.version == release.tag.name {
                     true => println!("latest: name={}/{} tag={}",
@@ -134,7 +105,7 @@ fn main() {
     info!("Bye");
 }
 
-fn retrieve_releases(host: &str, github_token: &str, owner: &str, name: &str) -> ResultBody {
+fn retrieve_releases(host: &str, github_token: &str, owner: &str, name: &str) -> String {
     let mut client_builder = reqwest::ClientBuilder::new();
 
     if let Some(proxy) = get_proxy() {
@@ -156,7 +127,7 @@ fn retrieve_releases(host: &str, github_token: &str, owner: &str, name: &str) ->
             }
         }).to_string())
         .send().unwrap()
-        .json::<ResultBody>().unwrap()
+        .text().unwrap()
 }
 
 fn get_github_token() -> Option<String> {
