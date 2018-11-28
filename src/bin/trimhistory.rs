1f719bee90ae97ee01b64f0c9a0b1495da3f7d7f
Satoru Sukawa <sukawasatoru.github@outlook.jp>
2018-11-29 01:03:35 +0900

    Implements new feature that store trimmed count

--- src/bin/trimhistory.rs
+++ src/bin/trimhistory.rs
@@ -1,12 +1,16 @@
+extern crate directories;
 extern crate dotenv;
 extern crate env_logger;
 #[macro_use]
 extern crate log;
+#[macro_use]
+extern crate serde_derive;
 extern crate structopt;
+extern crate toml;
 
 use std::fs::File;
-use std::io::{BufRead, BufReader, BufWriter, Write};
-use std::path::PathBuf;
+use std::io::{BufRead, BufReader, BufWriter, Read, Write};
+use std::path::{Path, PathBuf};
 
 use structopt::StructOpt;
 
@@ -23,6 +27,43 @@ struct Opt {
     history_path: PathBuf,
 }
 
+#[derive(Debug, Deserialize, Serialize)]
+struct Entry {
+    command: String,
+    count: i32,
+}
+
+impl Entry {
+    fn new(command: &str) -> Entry {
+        Entry {
+            command: command.to_owned(),
+            count: 1,
+        }
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Statistics {
+    entries: Vec<Entry>,
+}
+
+impl Statistics {
+    fn new() -> Statistics {
+        Statistics {
+            entries: Vec::new(),
+        }
+    }
+
+    fn find_command(&self, command: &str) -> Result<usize, ()> {
+        for i in 0..self.entries.len() {
+            if self.entries[i].command == command {
+                return Ok(i)
+            }
+        }
+        Err(())
+    }
+}
+
 fn main() {
     dotenv::dotenv().ok();
     env_logger::init();
@@ -34,6 +75,15 @@ fn main() {
 
     let file_path = opt.history_path;
     debug!("input {:?}", file_path);
+    let project_dirs = directories::ProjectDirs::from(
+        "jp", "tinyport", "trimhistory").unwrap();
+    let statistics_path = project_dirs.data_dir().join("statistics.toml");
+
+    let mut statistics = if statistics_path.exists() {
+        load_statistics(&statistics_path).unwrap()
+    } else {
+        Statistics::new()
+    };
 
     let history_file = File::open(&file_path).unwrap();
     let mut buffer = BufReader::new(&history_file);
@@ -57,6 +107,7 @@ fn main() {
                         debug!("contains: {}", index);
                         trimmed.remove(index);
                         trim_count += 1;
+                        increment_command_count(&mut statistics, trimmed_line);
                     }
                     trimmed.push(trimmed_line.to_owned());
                 }
@@ -78,5 +129,54 @@ fn main() {
     }
     writer.flush().unwrap();
 
+    store_statistics(&statistics_path, &statistics).unwrap();
+
     info!("Bye");
+}
+
+fn load_statistics(path: &Path) -> Result<Statistics, ()> {
+    let statistics_file = match File::open(&path) {
+        Ok(file) => file,
+        Err(e) => panic!(e),
+    };
+    let mut buf = BufReader::new(statistics_file);
+    let mut statistics_data = Vec::new();
+    if let Err(e) = buf.read_to_end(&mut statistics_data) {
+        panic!(e);
+    }
+    match toml::from_slice::<Statistics>(&statistics_data) {
+        Ok(ok) => Ok(ok),
+        Err(e) => panic!(e),
+    }
+}
+
+fn store_statistics(path: &Path, statistics: &Statistics) -> Result<(), ()> {
+    use std::fs;
+    let data_dir = path.parent().unwrap();
+    if !data_dir.exists() {
+        if let Err(e) = fs::create_dir_all(data_dir) {
+            panic!(e);
+        }
+    }
+    match File::create(&path) {
+        Ok(file) => {
+            let mut writer = BufWriter::new(file);
+            let statistics_data = match toml::to_vec(&statistics) {
+                Ok(data) => data,
+                Err(e) => panic!(e),
+            };
+            if let Err(e) = writer.write_all(&statistics_data) {
+                panic!(e);
+            }
+            Ok(())
+        }
+        Err(e) => panic!(e),
+    }
+}
+
+fn increment_command_count(statistics: &mut Statistics, command: &str) {
+    match statistics.find_command(command) {
+        Ok(index) => statistics.entries[index].count += 1,
+        Err(_) => statistics.entries.push(Entry::new(command)),
+    }
 }
