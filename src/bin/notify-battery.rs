522e0fbd9bc2c5a6beeea91d3436ce9f48d751b2
Satoru Sukawa <sukawasatoru.github@outlook.jp>
2020-01-07 00:47:44 +0900

    Battery utility

---
+++ src/bin/notify-battery.rs
@@ -0,0 +1,167 @@
+use log::{debug, info};
+use regex::Regex;
+use rust_myscript::myscript::prelude::*;
+use std::io::BufRead;
+use structopt::StructOpt;
+
+struct Context {
+    terminal_notifier_name: String,
+    slack_user_name: String,
+    slack_notify_url: String,
+    reqwest_client: reqwest::Client,
+    battery_level_regex: Regex,
+    battery_remaining_regex: Regex,
+}
+
+struct PSInfo {
+    battery_level: u8,
+    battery_remaining: String,
+}
+
+#[derive(Debug, StructOpt)]
+struct Opt {
+    /// Bot name for slack
+    #[structopt(long)]
+    slack_bot_name: String,
+
+    /// Web hooks URL for slack
+    #[structopt(long)]
+    slack_notify_url: String,
+}
+
+fn main() -> Fallible<()> {
+    dotenv::dotenv().ok();
+    env_logger::init();
+
+    info!("Hello");
+
+    let opt: Opt = Opt::from_args();
+
+    let notify_threshold = 40;
+    let context = Context {
+        terminal_notifier_name: "terminal-notifier".to_owned(),
+        slack_user_name: opt.slack_bot_name,
+        slack_notify_url: opt.slack_notify_url,
+        reqwest_client: reqwest::Client::new(),
+        battery_level_regex: Regex::new("	([0-9]*)%;")?,
+        battery_remaining_regex: Regex::new("; ([0-9:]*) remaining present: true")?,
+    };
+
+    let use_terminal_notifier = check_terminal_notifier(&context.terminal_notifier_name);
+    debug!("use_terminal_notifier: {}", use_terminal_notifier);
+
+    let process = std::process::Command::new("pmset")
+        .args(&["-g", "pslog"])
+        .stdin(std::process::Stdio::piped())
+        .stdout(std::process::Stdio::piped())
+        .spawn()?;
+
+    let mut reader = std::io::BufReader::new(process.stdout.ok_or_err()?);
+    let mut s = String::new();
+    loop {
+        debug!("loop");
+        s.clear();
+        let read_result = reader.read_line(&mut s)?;
+        if read_result == 0 {
+            break;
+        }
+
+        print!("{}", s);
+        let ps_info = match parse_line(&context, &s) {
+            Some(data) => data,
+            None => continue,
+        };
+        info!(
+            "battery_level: {}, remaining: {}",
+            ps_info.battery_level, ps_info.battery_remaining
+        );
+
+        if ps_info.battery_level <= notify_threshold {
+            if use_terminal_notifier {
+                notify_terminal(&context, &ps_info).ok();
+            }
+
+            match notify_slack(&context, &ps_info) {
+                Ok(data) => info!("notify success: {}", data),
+                Err(e) => info!("notify fail: {}", e),
+            };
+        }
+    }
+
+    info!("Bye");
+
+    Ok(())
+}
+
+fn parse_line(context: &Context, line: &str) -> Option<PSInfo> {
+    let battery_level = match context.battery_level_regex.captures(line) {
+        Some(data) => match data.get(1).unwrap().as_str().parse() {
+            Ok(data) => data,
+            Err(_) => return None,
+        },
+        None => return None,
+    };
+
+    let battery_remaining = match context.battery_remaining_regex.captures(line) {
+        Some(data) => data.get(1).unwrap().as_str().to_owned(),
+        None => return None,
+    };
+
+    Some(PSInfo {
+        battery_level,
+        battery_remaining,
+    })
+}
+
+fn check_terminal_notifier(executable_name: &str) -> bool {
+    if let Ok(_) = std::env::var("SSH_TTY") {
+        debug!("has SSH_TTY");
+        return false;
+    }
+
+    match std::process::Command::new("command")
+        .args(&["-v", executable_name])
+        .status()
+    {
+        Ok(status) => status.success(),
+        Err(_) => false,
+    }
+}
+
+fn notify_terminal(context: &Context, ps_info: &PSInfo) -> Fallible<()> {
+    std::process::Command::new(&context.terminal_notifier_name)
+        .args(&vec![
+            "-message",
+            &format!("Battery: {}", ps_info.battery_level),
+        ])
+        .status()?;
+
+    Ok(())
+}
+
+fn notify_slack(context: &Context, ps_info: &PSInfo) -> Fallible<String> {
+    debug!("payload: {}", generate_slack_payload(&context, &ps_info));
+    let ret = context
+        .reqwest_client
+        .post(&context.slack_notify_url)
+        .body(generate_slack_payload(&context, &ps_info))
+        .header(
+            reqwest::header::CONTENT_TYPE,
+            reqwest::header::HeaderValue::from_str("application/x-www-form-urlencoded")?,
+        )
+        .send()?
+        .text()?;
+
+    Ok(ret)
+}
+
+fn generate_slack_payload(context: &Context, ps_info: &PSInfo) -> String {
+    format!(
+        r#"payload={{
+    "icon_emoji": ":computer:",
+    "username": "{}",
+    "text": "Battery: {}%, {} remaining present"
+}}"#,
+        context.slack_user_name, ps_info.battery_level, ps_info.battery_remaining
+    )
+}
