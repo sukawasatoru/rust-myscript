bac4f02c9c471b86413d13443515754f2282959b
Satoru Sukawa <sukawasatoru.github@outlook.jp>
2021-01-30 04:54:52 +0900

    Add TLS support

--- src/bin/webfile.rs
+++ src/bin/webfile.rs
@@ -1,22 +1,52 @@
+use rust_myscript::prelude::*;
 use std::path::PathBuf;
 use structopt::StructOpt;
 
 #[derive(StructOpt)]
 struct Opt {
-    /// Directory to root
-    #[structopt(parse(from_os_str), default_value = ".")]
-    dir: PathBuf,
-
     /// Port number
     #[structopt(short, long, default_value = "38080")]
     port: u16,
+
+    /// Path of the fullchain.pem / This flag requires "key-path" flag
+    #[structopt(short, long, parse(from_os_str))]
+    cert_path: Option<PathBuf>,
+
+    /// Path of the privkey.pem / This flag requires "cert-path" flag
+    #[structopt(short, long, parse(from_os_str))]
+    key_path: Option<PathBuf>,
+
+    /// Directory to root
+    #[structopt(parse(from_os_str), default_value = ".")]
+    dir: PathBuf,
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Fallible<()> {
     let opt: Opt = Opt::from_args();
 
-    warp::serve(warp::fs::dir(opt.dir))
-        .run(([0, 0, 0, 0], opt.port))
-        .await;
+    let cert_args: Option<(PathBuf, PathBuf)> = match (opt.cert_path, opt.key_path) {
+        (Some(cert_path), Some(key_path)) => Some((cert_path, key_path)),
+        (None, None) => None,
+        _ => {
+            anyhow::bail!(
+                "The cert argument requires pair of the \"--cert-path\" and \"--key-path\""
+            )
+        }
+    };
+
+    let addr = ([0, 0, 0, 0], opt.port);
+
+    if let Some((cert_path, key_path)) = cert_args {
+        warp::serve(warp::fs::dir(opt.dir))
+            .tls()
+            .cert_path(cert_path)
+            .key_path(key_path)
+            .run(addr)
+            .await;
+    } else {
+        warp::serve(warp::fs::dir(opt.dir)).run(addr).await;
+    }
+
+    Ok(())
 }
