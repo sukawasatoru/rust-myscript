1043cd4063741b2f718dc20384dc30e3b9e8df43
Satoru Sukawa <sukawasatoru.github@outlook.jp>
2024-04-13 17:39:53 +0900

    Tweaks

--- src/bin/unzip-bt.rs
+++ src/bin/unzip-bt.rs
@@ -18,7 +18,8 @@ use clap::{Parser, ValueHint};
 use indicatif::HumanDuration;
 use rust_myscript::prelude::*;
 use std::fs::File;
-use std::io::{BufReader, Read};
+use std::io::prelude::*;
+use std::io::BufReader;
 use std::path::PathBuf;
 use std::sync::{Arc, Mutex};
 use zip::result::ZipError;
@@ -26,6 +27,12 @@ use zip::ZipArchive;
 
 #[derive(Parser)]
 struct Opt {
+    #[clap(short, long, default_value = "2000")]
+    update_interval: usize,
+
+    #[clap(short, long, default_value_t = num_cpus::get() + 1)]
+    threads: usize,
+
     /// Zip file.
     #[clap(value_hint = ValueHint::FilePath)]
     file: PathBuf,
@@ -51,84 +58,22 @@ fn main() -> Fallible<()> {
             Err(e) => Err(e).context("failed to get file")?,
         };
 
-        let output_path = match entry.enclosed_name() {
-            Some(data) => data,
-            None => {
-                info!(name = %entry.name(), "Entry has a suspicious path");
-                continue;
-            }
-        };
-
-        {
-            let comment = entry.comment();
-            if !comment.is_empty() {
-                info!(%comment, name = %entry.name());
-            }
-        }
-
         if entry.is_dir() {
-            debug!(entry = %output_path.display(), "directory");
+            debug!(entry = ?entry.enclosed_name(), "directory");
         }
     }
 
     let (tx, rx) = tokio::sync::watch::channel(false);
-    let base_password = Mutex::new(vec![]);
-    let next_base_password = Arc::new(move |password: Option<Vec<u8>>| {
-        fn inc_carry_up(start_index: usize, end_index: usize, password: &mut [u8]) -> (bool, bool) {
-            for (index, entry) in password
-                .iter_mut()
-                .enumerate()
-                .take(end_index + 1)
-                .skip(start_index)
-            {
-                if entry == &b'~' {
-                    *entry = b' ';
-                    if index == end_index {
-                        return (true, true);
-                    }
-                } else {
-                    password[index] += 1;
-                    return (false, index == end_index);
-                }
-            }
-            (false, true)
-        }
-
-        match password {
-            Some(mut password) => {
-                let ret = inc_carry_up(0, password.len() - 1, &mut password);
-                match ret {
-                    (true, _) | (_, true) => {
-                        let mut pw = base_password.lock().unwrap();
-                        let (overflow, _) = inc_carry_up(pw.len() - 1, pw.len() - 1, &mut pw);
-                        if overflow {
-                            pw.push(b' ');
-                        }
-                        pw.clone()
-                    }
-                    (false, false) => password,
-                }
-            }
-            None => {
-                let mut pw = base_password.lock().unwrap();
-                if pw.is_empty() {
-                    pw.push(b' ');
-                } else {
-                    let (overflow, _) = inc_carry_up(0, pw.len() - 1, &mut pw);
-                    if overflow {
-                        pw.push(b' ');
-                    }
-                }
-                pw.clone()
-            }
-        }
-    });
+    let next_base_password = Arc::new(next_password_generator());
 
     let start_time = std::time::Instant::now();
     let bars = indicatif::MultiProgress::new();
     let mut threads = vec![];
-    let index = *file_indexes.first().unwrap();
-    for i in 0..=num_cpus::get() {
+    let index = match file_indexes.first() {
+        Some(data) => *data,
+        None => return Ok(()),
+    };
+    for i in 0..opt.threads {
         let tx = tx.clone();
         let rx = rx.clone();
         let next_password = next_base_password.clone();
@@ -146,7 +91,7 @@ fn main() -> Fallible<()> {
                 }
 
                 update_counter += 1;
-                if 1000 < update_counter {
+                if opt.update_interval < update_counter {
                     update_counter = 0;
                     bar.set_message(format!(
                         "{bytes} {password}",
@@ -189,6 +134,7 @@ fn main() -> Fallible<()> {
         });
         threads.push(t);
     }
+
     for t in threads {
         t.join().unwrap();
     }
@@ -196,6 +142,58 @@ fn main() -> Fallible<()> {
     println!("{}", HumanDuration(start_time.elapsed()));
 
     Ok(())
+}
+
+fn next_password_generator() -> impl Fn(Option<Vec<u8>>) -> Vec<u8> {
+    fn inc_carry_up(start_index: usize, end_index: usize, password: &mut [u8]) -> (bool, bool) {
+        for (index, entry) in password
+            .iter_mut()
+            .enumerate()
+            .take(end_index + 1)
+            .skip(start_index)
+        {
+            if entry == &b'~' {
+                *entry = b' ';
+                if index == end_index {
+                    return (true, true);
+                }
+            } else {
+                *entry += 1;
+                return (false, index == end_index);
+            }
+        }
+        (false, true)
+    }
+
+    let base_password = Mutex::new(vec![]);
+    move |password: Option<Vec<u8>>| match password {
+        Some(mut password) => {
+            let ret = inc_carry_up(0, password.len() - 1, &mut password);
+            match ret {
+                (true, _) | (_, true) => {
+                    let mut pw = base_password.lock().unwrap();
+                    let (overflow, _) = inc_carry_up(pw.len() - 1, pw.len() - 1, &mut pw);
+                    if overflow {
+                        pw.push(b' ');
+                    }
+                    pw.clone()
+                }
+                (false, false) => password,
+            }
+        }
+        None => {
+            let mut pw = base_password.lock().unwrap();
+            if pw.is_empty() {
+                pw.push(b' ');
+            } else {
+                let (overflow, _) = inc_carry_up(pw.len() - 1, pw.len() - 1, &mut pw);
+                if overflow {
+                    pw.push(b' ');
+                }
+            }
+            pw.clone()
+        }
+    }
 }
 
 #[cfg(test)]
