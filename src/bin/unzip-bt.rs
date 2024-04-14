d37b9d8e0a81390040deef4260203f287a6a3417
Satoru Sukawa <sukawasatoru.github@outlook.jp>
2024-04-14 17:37:05 +0900

    Add test

--- src/bin/unzip-bt.rs
+++ src/bin/unzip-bt.rs
@@ -219,4 +219,55 @@ mod tests {
     fn verify_cli() {
         Opt::command().debug_assert();
     }
+
+    #[test]
+    fn next_password_generator_0() {
+        let gen = next_password_generator(0);
+        let ascii_list = ascii_list();
+
+        for value in ascii_list.clone() {
+            assert_eq!(gen(None), vec![value]);
+        }
+        assert_eq!(gen(None), vec![ascii_list[0], ascii_list[0]]);
+        assert_eq!(gen(None), vec![ascii_list[0], ascii_list[1]]);
+        assert_eq!(
+            gen(Some(vec![ascii_list[0], ascii_list[1]])),
+            vec![ascii_list[1], ascii_list[1]]
+        );
+        assert_eq!(gen(None), vec![ascii_list[0], ascii_list[2]]);
+    }
+
+    #[test]
+    fn next_password_generator_1() {
+        let gen = next_password_generator(1);
+        let ascii_list = ascii_list();
+
+        for value in ascii_list.clone() {
+            assert_eq!(gen(None), vec![value]);
+        }
+        assert_eq!(gen(None), vec![ascii_list[0], ascii_list[0]]);
+    }
+
+    #[test]
+    fn next_password_generator_2() {
+        let gen = next_password_generator(2);
+        let ascii_list = ascii_list();
+
+        for value in ascii_list.clone() {
+            assert_eq!(gen(None), vec![b' ', value]);
+        }
+        assert_eq!(gen(None), vec![ascii_list[0], ascii_list[0], ascii_list[0]]);
+    }
+
+    fn ascii_list() -> Vec<u8> {
+        vec![
+            b' ', b'!', b'"', b'#', b'$', b'%', b'&', b'\'', b'(', b')', b'*', b'+', b',', b'-',
+            b'.', b'/', b'0', b'1', b'2', b'3', b'4', b'5', b'6', b'7', b'8', b'9', b':', b';',
+            b'<', b'=', b'>', b'?', b'@', b'A', b'B', b'C', b'D', b'E', b'F', b'G', b'H', b'I',
+            b'J', b'K', b'L', b'M', b'N', b'O', b'P', b'Q', b'R', b'S', b'T', b'U', b'V', b'W',
+            b'X', b'Y', b'Z', b'[', b'\\', b']', b'^', b'_', b'`', b'a', b'b', b'c', b'd', b'e',
+            b'f', b'g', b'h', b'i', b'j', b'k', b'l', b'm', b'n', b'o', b'p', b'q', b'r', b's',
+            b't', b'u', b'v', b'w', b'x', b'y', b'z', b'{', b'|', b'}', b'~',
+        ]
+    }
 }
